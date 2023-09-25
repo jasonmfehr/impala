d847e1a4d3bad7b62bfd38acd510ddea7b872ee8
jasonmfehr <jfehr@cloudera.com>
2023-09-25 13:32:41 -0700

    query successfully finishes
    Change-Id: Ia763eac50f9c42829fa7e1fe00311285413e8824

--- be/src/service/impala-server.cc
+++ be/src/service/impala-server.cc
@@ -3084,32 +3084,6 @@ void ImpalaServer::UnregisterSessionTimeout(int32_t session_timeout) {
       handle->Wait();
       LOG(INFO) << "DONE WAITING" << std::endl;
 
-      // const TResultSetMetadata* result_set_md = handle->result_metadata();
-      // DCHECK(result_set_md != NULL);
-      // for (int i=0; i<result_set_md->columns.size(); i++) {
-      //   LOG(INFO) << "CREATE TABLE RESULT METADATA" << std::endl;
-      //   result_set_md->printTo(LOG(INFO));
-      // }
-
-      // handle->set_fetched_rows();
-      // switch (handle->exec_state()) {
-      //   case ClientRequestState::ExecState::ERROR:
-      //     LOG(INFO) << "QUERY STATE: ERROR" << std::endl;
-      //     break;
-      //   case ClientRequestState::ExecState::INITIALIZED:
-      //     LOG(INFO) << "QUERY STATE: INITIALIZED" << std::endl;
-      //     break;
-      //   case ClientRequestState::ExecState::RUNNING:
-      //     LOG(INFO) << "QUERY STATE: RUNNING" << std::endl;
-      //     break;
-      //   case ClientRequestState::ExecState::FINISHED:
-      //     LOG(INFO) << "QUERY STATE: FINISHED" << std::endl;
-      //     break;
-      //   case ClientRequestState::ExecState::PENDING:
-      //     LOG(INFO) << "QUERY STATE: PENDING" << std::endl;
-      //     break;
-      // }
-
       shared_ptr<vector<string>> row_set = make_shared<vector<string>>();
       auto result_set = QueryResultSet::CreateAsciiQueryResultSet(*handle->result_metadata(), row_set.get(), true);
       while (!handle->eos()) {
@@ -3119,12 +3093,9 @@ void ImpalaServer::UnregisterSessionTimeout(int32_t session_timeout) {
         }
       }
 
-      /*
       stat = this->UnregisterQuery(query_context.query_id, true);
       LOG(INFO) << "CREATE TABLE UNREGISTER QUERY CODE: " << stat.code() << std::endl;
-      */
 
-      // this->FinishUnregisterQuery(handle);
       this->MarkSessionInactive(session_state);
     }
   }
