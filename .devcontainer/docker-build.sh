ab29c11e5abe5460dd7c9cc77080074020a695fc
jfehr <jfehr@cloudera.com>
2024-12-16 08:59:11 -0800

    run on generic host

--- .devcontainer/docker-build.sh
+++ .devcontainer/docker-build.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
+IMPALA_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/..
+
 DOCKER_IMG="${2-impaladev}:${1-latest}"
 export DOCKER_DEFAULT_PLATFORM=linux/amd64
 
-# onexit() {
-#   popd
-# }
-
-# pushd ..
-# trap onexit EXIT
-DOCKER_BUILDKIT=1 docker build --force-rm --progress=plain --add-host="impdev:127.0.0.1" --tag="${DOCKER_IMG}" -f .devcontainer/Dockerfile /Users/jfehr/dev/apache/impala/
+docker build --force-rm --progress=plain --add-host="impdev:127.0.0.1" --tag="${DOCKER_IMG}" -f .devcontainer/Dockerfile "${IMPALA_ROOT}"
