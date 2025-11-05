1b5f5e8bb4a266953d5843114f5d2566fa3ee53c
jasonmfehr <jfehr@cloudera.com>
2025-11-05 10:52:41 -0800

    tar up dependencies

--- .devcontainer-build/build-devcontainer.sh
+++ .devcontainer-build/build-devcontainer.sh
@@ -3,4 +3,12 @@ set -euo pipefail
 
 IMPALA_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/..
 
-devcontainer build --workspace-folder="${IMPALA_ROOT}" --config=".devcontainer-build/devcontainer.json"  --image-name="jasonmfehr/impaladev:latest" --platform "linux/amd64"
+if [[ -z "${PLATFORM:-}" ]]; then
+  if [[ "$(uname -m)" == "x86_64" ]]; then
+    PLATFORM="linux/amd64"
+  else
+    PLATFORM="linux/arm64"
+  fi
+fi
+
+devcontainer build --workspace-folder="${IMPALA_ROOT}" --config=".devcontainer-build/devcontainer.json"  --image-name="jasonmfehr/impaladev:latest" --platform "${PLATFORM}"
