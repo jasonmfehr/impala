9ccebb838ae02e34d3f610d2066598e7ef7be380
jasonmfehr <jfehr@cloudera.com>
2025-11-06 11:25:56 -0800

    refined into a version that works with a docker volume

--- .devcontainer-build/build-devcontainer.sh
+++ .devcontainer-build/build-devcontainer.sh
@@ -11,4 +11,13 @@ if [[ -z "${PLATFORM:-}" ]]; then
   fi
 fi
 
-devcontainer build --workspace-folder="${IMPALA_ROOT}" --config=".devcontainer-build/devcontainer.json"  --image-name="jasonmfehr/impaladev:latest" --platform "${PLATFORM}"
+. "${IMPALA_ROOT}/bin/impala-config.sh"
+devcontainer \
+  build \
+  --workspace-folder="${IMPALA_ROOT}" \
+  --config=".devcontainer-build/devcontainer.json" \
+  --image-name="jasonmfehr/impaladev:latest" \
+  --platform "${PLATFORM}" \
+  --build-arg "IMPALA_CMAKE_VERSION=${IMPALA_CMAKE_VERSION}" \
+  --build-arg "IMPALA_GCC_VERSION=${IMPALA_GCC_VERSION}" \
+  --build-arg "TOOLCHAIN_PACKAGES_RELATIVE_PATH=${IMPALA_TOOLCHAIN_PACKAGES_HOME#"${IMPALA_HOME}"}"
