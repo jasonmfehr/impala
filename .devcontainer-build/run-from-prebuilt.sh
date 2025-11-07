22f494e575dc4a7895c3a9ac994551130d471729
jasonmfehr <jfehr@cloudera.com>
2025-11-06 16:15:16 -0800

    more improvements

--- .devcontainer-build/run-from-prebuilt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-docker rm impaladevctr -f || true
-docker volume rm impaladevctr -f || true
-docker run -d --name=impaladevctr jasonmfehr/impaladev:latest
-devcontainer set-up --container-id impaladevctr --config=/home/jfehr/impala/.devcontainer/devcontainer.json
