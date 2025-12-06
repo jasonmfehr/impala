d76a4b2350575909278204635112b3ea5788c051
jasonmfehr <jfehr@cloudera.com>
2025-12-05 16:27:23 -0800

    save point

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 set -euo pipefail
 
+echodate() {
+  echo "[$(date +'%Y-%m-%d %H:%M:%S')] $*"
+}
+
+echodate "Starting container initialization..."
+
 if [[ $(grep -cE "127.0.0.1\s+$(hostname)" /etc/hosts || true) == "0" ]]; then
-  echo "Adding container hostname '$(hostname)]' to /etc/hosts..."
+  echodate "Adding container hostname '$(hostname)]' to /etc/hosts..."
   echo "127.0.0.1 $(hostname) $(hostname -s)" | sudo tee -a /etc/hosts
   echo "DNS setup complete."
 fi
 
-echo "Setting up SSH..."
+echodate "Setting up SSH..."
 /bin/bash -c 'sudo ssh-keygen -A; sudo service ssh start'
-echo "SSH setup complete."
+echodate "SSH setup complete."
 
-echo "Starting PostgreSQL..."
+echodate "Starting PostgreSQL..."
 sudo service postgresql start
-echo "PostgreSQL started."
+echodate "PostgreSQL started."
 
-echo "Starting supporting services..."
+echodate "Starting supporting services..."
 cd "${IMPALA_HOME}"
 . bin/impala-config.sh
 . bin/set-classpath.sh
 ./testdata/bin/run-all.sh
-echo "Supporting services started."
+echodate "Supporting services started."
 
 echo
-echo "Initialization finished. Container is ready"
-sleep infinity
+echodate "Initialization finished. Container is ready"
